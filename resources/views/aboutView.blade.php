d0eccf56a4b21438d0454117ca03901bd675e637
nahidhasanweb777 <nahidhasanweb777@gmail.com>
2022-06-05 07:00:15 -0700

    laravel install

---
+++ resources/views/aboutView.blade.php
@@ -0,0 +1,3 @@
+<h1> My name is Nahid Hasan</h1>
+<h2> I am from bangladesh </h2>
+<h3> I am 23 years old</h3>
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/name', function(){
+   return "<h1>my name is nahid</h1>";
+});
+
+Route :: get('/about',function(){
+    return view('aboutView');
+});
+
+Route :: get('/contact','newController@contact');
